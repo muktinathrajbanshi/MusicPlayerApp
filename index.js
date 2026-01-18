a3d29f31c07e3e0cb5341205ecbdfbf1966b43f1
unknown <muktinathrajbanshi786@gmail.com>
2026-01-18 20:06:45 +0545

    wow it's fantastic man

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const music = document.querySelector("audio");
 const play = document.getElementById("play");
+const img = document.querySelector("img");
 
 play.addEventListener("click", () => {
     music.play();
+    play.classList.replace("fa-play", "fa-pause");
+    img.classList.add("anime");
+     
 })
