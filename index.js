d4f1a0077b6bd2d1bcd0acbdccd9dbb51b38cc91
unknown <muktinathrajbanshi786@gmail.com>
2026-01-18 20:19:23 +0545

    wow it's awesome job man

--- index.js
+++ index.js
@@ -2,9 +2,29 @@ const music = document.querySelector("audio");
 const play = document.getElementById("play");
 const img = document.querySelector("img");
 
-play.addEventListener("click", () => {
+let isPlaying = false;
+
+// for play function 
+ playMusic = () => {
+    isPlaying = true;
     music.play();
     play.classList.replace("fa-play", "fa-pause");
-    img.classList.add("anime");
-     
+    img.classList.add("anime");   
+}
+
+// for pause functionality 
+
+const pauseMusic = () => {
+    isPlaying = false;
+    music.pause();
+    play.classList.replace("fa-pause", "fa-play");
+    img.classList.remove("anime");   
+}
+
+play.addEventListener("click", () => {
+    if(isPlaying) {
+        pauseMusic();
+    } else {
+        playMusic();
+    }
 })
